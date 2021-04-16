40fcf1d8539d07b193ba7de519e2e3d18f1979e9
Spakelios <73300254+Spakelios@users.noreply.github.com>
2021-04-16 16:15:03 +0100

    UI and collectables

---
+++ vision-Spakelios-main/vision-Spakelios-main/Vision 2d/Assets/Code/ScoreManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public TextMeshProUGUI text;
+    private int score;
+   
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void ChangeScore(int dropValue)
+    {
+        score += dropValue;
+        text.text = "X" + score.ToString();
+    }
+}
