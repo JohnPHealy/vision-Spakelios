01bbca6ef3aa8bbba7c7ba9d464b4214a637f591
Spakelios <73300254+Spakelios@users.noreply.github.com>
2021-04-25 19:02:02 +0100

    dialogue triggers

--- vision-Spakelios-main/vision-Spakelios-main/Vision 2d/Assets/Code/EyeMovement.cs
+++ vision-Spakelios-main/vision-Spakelios-main/Vision 2d/Assets/Code/EyeMovement.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EyeMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-   
+
     public float speed;
     private Vector2 targetPosition;
+
     void Start()
     {
-        
+
         targetPosition = new Vector2(5f, 5f);
     }
 
@@ -22,16 +25,18 @@ public class EyeMovement : MonoBehaviour
             targetPosition = Input.mousePosition;
             targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(targetPosition.x, targetPosition.y, 0.0f));
         }
+
         this.transform.position = Vector2.MoveTowards(this.transform.position, targetPosition, speed * Time.deltaTime);
-           
-       
+
+
     }
-    
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Eye")) 
+        if (other.gameObject.CompareTag("Eye"))
         {
             gameObject.GetComponent<Animator>().Play("Eye fixed");
         }
     }
+    
 }
